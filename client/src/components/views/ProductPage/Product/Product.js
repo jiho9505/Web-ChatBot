4228b69168fa6baf87ce948488ac2ca8fb5bd79e
jiho9505 <sun_days@naver.com>
2021-02-07 11:57:59 +0900

    Uploaded Images

--- client/src/components/views/ProductPage/Product/Product.js
+++ client/src/components/views/ProductPage/Product/Product.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import { Col, Card, Row } from 'antd';
-import ProductImage from '../ProductImage/sample.jpg';
+import S1 from '../ProductImage/S1.jpg';
+import S2 from '../ProductImage/S2.jpg';
+import S3 from '../ProductImage/S3.jpg';
+import S4 from '../ProductImage/S4.jpg';
+import S5 from '../ProductImage/S5.jpg';
+import S6 from '../ProductImage/S6.jpg';
+import S7 from '../ProductImage/S7.jpg';
+import S8 from '../ProductImage/S8.jpg';
+import S9 from '../ProductImage/S9.jpg';
+import S10 from '../ProductImage/S10.jpg';
+import S11 from '../ProductImage/S11.jpg';
+import S12 from '../ProductImage/S12.jpg';
+import S13 from '../ProductImage/S13.jpg';
+
 
 function Product() {
 
@@ -10,34 +23,83 @@ function Product() {
 
                 <Col lg={6} md={8} xs={12}>
                     <Card
-                        cover={<img src={`${ProductImage}`} />}
+                        cover={<img src={`${S13}`} />}
                     >
                     </Card>       
                 </Col>
                 <Col lg={6} md={8} xs={12}>
                     <Card
-                        cover={<img src={`${ProductImage}`} />}
+                        cover={<img src={`${S12}`} />}
                     >
                     </Card>       
                 </Col>
                 <Col lg={6} md={8} xs={12}>
                     <Card
-                        cover={<img src={`${ProductImage}`} />}
+                        cover={<img src={`${S11}`} />}
                     >
                     </Card>       
                 </Col>
                 <Col lg={6} md={8} xs={12}>
                     <Card
-                        cover={<img src={`${ProductImage}`} />}
+                        cover={<img src={`${S10}`} />}
                     >
                     </Card>       
                 </Col>
                 <Col lg={6} md={8} xs={12}>
                     <Card
-                        cover={<img src={`${ProductImage}`} />}
+                        cover={<img src={`${S9}`} />}
                     >
                     </Card>       
                 </Col>
+                <Col lg={6} md={8} xs={12}>
+                    <Card
+                        cover={<img src={`${S8}`} />}
+                    >
+                    </Card>       
+                </Col>
+                <Col lg={6} md={8} xs={12}>
+                    <Card
+                        cover={<img src={`${S7}`} />}
+                    >
+                    </Card>       
+                </Col>
+                <Col lg={6} md={8} xs={12}>
+                    <Card
+                        cover={<img src={`${S6}`} />}
+                    >
+                    </Card>       
+                </Col>
+                <Col lg={6} md={8} xs={12}>
+                    <Card
+                        cover={<img src={`${S5}`} />}
+                    >
+                    </Card>       
+                </Col>
+                <Col lg={6} md={8} xs={12}>
+                    <Card
+                        cover={<img src={`${S4}`} />}
+                    >
+                    </Card>       
+                </Col>
+                <Col lg={6} md={8} xs={12}>
+                    <Card
+                        cover={<img src={`${S3}`} />}
+                    >
+                    </Card>       
+                </Col>
+                <Col lg={6} md={8} xs={12}>
+                    <Card
+                        cover={<img src={`${S2}`} />}
+                    >
+                    </Card>       
+                </Col>
+                <Col lg={6} md={8} xs={12}>
+                    <Card
+                        cover={<img src={`${S1}`} />}
+                    >
+                    </Card>       
+                </Col>
+
 
             </Row>
             
