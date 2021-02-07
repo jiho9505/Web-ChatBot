cad0dc38e771af13e66dde36f2dafb555ca44f82
jiho9505 <sun_days@naver.com>
2021-02-07 14:09:40 +0900

    Finished Product Page

--- client/src/components/views/ProductPage/ProductPage.js
+++ client/src/components/views/ProductPage/ProductPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Product from './Product/Product';
+import { Typography } from "antd";
 
+const { Title } = Typography
 
 function ProductPage() {
 
@@ -11,6 +13,7 @@ function ProductPage() {
             제품 샘플
         </h2>
         <div className='grayBorder'/>
+        <Title level={5}>이외에도 더 다양한 제품들이 가능합니다!</Title>
         <br/>
         <Product />
         
