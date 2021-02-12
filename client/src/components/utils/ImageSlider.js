834866d2f59b16d81a1d3ba4837cbebc02779f2e
jiho9505 <sun_days@naver.com>
2021-02-12 17:05:58 +0900

    Update

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -9,21 +9,21 @@ function ImageSlider() {
         <div style={{ margin: 'auto', width: '100%', height: '100%' }}>
            
             <Carousel autoplay>
-                     
+            
                     <div>
-                    {a &&
+                   
                         <img className="Imgslider__img"
-                            src={a} alt="a" />}
+                            src='https://dsjumul.s3.ap-northeast-2.amazonaws.com/1.jpg' alt="a" />
                     </div>
                     <div>
-                    {b &&
+                    
                         <img className="Imgslider__img"
-                            src={b} alt="b" />}
+                            src='https://dsjumul.s3.ap-northeast-2.amazonaws.com/2.jpg' alt="b" />
                     </div>
                     <div >
-                    {c &&
+                   
                         <img className="Imgslider__img"
-                            src={c} alt="c" />}
+                            src='https://dsjumul.s3.ap-northeast-2.amazonaws.com/3.jpg' alt="c" />
                     </div>
                 
             </Carousel> 
